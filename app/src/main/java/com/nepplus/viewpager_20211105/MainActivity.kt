1a34551be93154d44a699040fb28690a42ab0620
김민성 <kmsz5813@naver.com>
2021-11-05 12:24:35 +0900

    메인화면 - 뷰페이저 / 어댑터 연결

--- app/src/main/java/com/nepplus/viewpager_20211105/MainActivity.kt
+++ app/src/main/java/com/nepplus/viewpager_20211105/MainActivity.kt
@@ -2,10 +2,32 @@ package com.nepplus.viewpager_20211105
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.viewpager_20211105.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValuse()
+
+
     }
+
+    fun setupEvents(){
+
+
+    }
+
+    fun setValuse(){
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
+
+    }
+
+
 }
