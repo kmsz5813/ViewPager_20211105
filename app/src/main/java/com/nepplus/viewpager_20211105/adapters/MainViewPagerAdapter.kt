f4683bc487204943096d00be79df9e5d9a1e84bb
김민성 <kmsz5813@naver.com>
2021-11-05 12:19:35 +0900

    메인화면 뷰페이저 내용 담당 -> 어댑터 클래스  - getCount : 총 몇장?  - getItem : 위치별 프래그먼트?

---
+++ app/src/main/java/com/nepplus/viewpager_20211105/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.nepplus.viewpager_20211105.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.nepplus.viewpager_20211105.fragments.HelloFragment
+import com.nepplus.viewpager_20211105.fragments.IntroduceFragment
+import com.nepplus.viewpager_20211105.fragments.NameFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        if (position == 0){
+            return HelloFragment()
+        }
+        else if (position == 1){
+            return NameFragment()
+        }
+        else {
+            return IntroduceFragment()
+        }
+    }
+}
